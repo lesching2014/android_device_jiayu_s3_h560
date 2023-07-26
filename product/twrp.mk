a24905d339b3834ba12ef316c48c4ad3d30fec21
lesching2014 <73586187+lesching2014@users.noreply.github.com>
2023-07-26 15:39:08 +0800

    Create twrp.mk

---
+++ product/twrp.mk
@@ -0,0 +1,21 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Copy updated tzdata
+PRODUCT_COPY_FILES += system/timezone/output_data/iana/tzdata:recovery/root/system_root/system/usr/share/zoneinfo/tzdata
+
+PRODUCT_PACKAGES += \
+    tzdata_twrp
