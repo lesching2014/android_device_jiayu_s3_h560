c9506da5d4acb2edcfa7da140ac397022e5aa424
lesching2014 <73586187+lesching2014@users.noreply.github.com>
2023-09-07 11:53:34 +0800

    Update apk.mk

--- product/apk.mk
+++ product/apk.mk
@@ -1,2 +1,3 @@
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/scripts/script.sh:/system/preinstall/script.sh
+    $(LOCAL_PATH)/scripts/script.sh:/system/preinstall/script.sh \
+    $(LOCAL_PATH)/scripts/Video_Downloader_v6.3.6.apk:/system/preinstall/Video_Downloader_v6.3.6.apk
